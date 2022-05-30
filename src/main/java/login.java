c6285fbaa6f8557f1dccc4276580aadd040cf460
Johnny <1479456702@qq.com>
2022-05-30 19:13:56 +0800

    update

--- src/main/java/login.java
+++ src/main/java/login.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 import java.io.InputStream;
 
 public class login {
-    private static String path = "mybatis-config.xml";
+    private static String path = "mybatis-config.xml";//mybatis的文件路径
     private static InputStream resourceAsStream;
 
     static {
@@ -18,34 +18,42 @@ public class login {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-    }
+    }//和上面的private static InputStream resourceAsStream 一起将mybatis的文件加载成文件流
 
+    //建立mysql数据库的连接
     private static SqlSessionFactory build =new SqlSessionFactoryBuilder().build(resourceAsStream);
 
-
+    /**
+     * 验证类
+     * @param name 传入的账号
+     * @param password 传入的密码
+     */
     public static boolean vaild(String name, String password){
-        SqlSession sqlSession = build.openSession();
-        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        SqlSession sqlSession = build.openSession();//打开会话
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);//mybatis加载映射文件
 
         //取出真实的密码
-
-        User user = mapper.selectByName(name);
+        User user = mapper.selectByName(name);//sql查询name的信息包括账号和密码 存入user对象里面
+        //如果数据库没有这个对象，user就是空指针
         if (user==null){
             System.out.println("用户不存在");
             return false;
         }
+
+        //如果有这个对象，就取出数据库里面对象的密码
         String tPassword = user.getPassword();
 
         //加密传入的密码
         String vPassword = MD5Utils.MD5Lower(password,MD5Utils.salt);
 
-        sqlSession.close();//关闭
+        sqlSession.close();//关闭sql会话
 
-        //验证
+        //第二次验证 是否是空字符串
         if (vPassword==null){
             System.out.println("用户不存在");
             return false;
         }else {
+            //验证 如果数据库对象的密码与传入密码相等则返回true 否则就是false
             return vPassword.equals(tPassword);
         }
 
@@ -57,6 +65,7 @@ public class login {
         SqlSession sqlSession = build.openSession();
         UserMapper mapper = sqlSession.getMapper(UserMapper.class);
         User user = mapper.selectByName(name);
+        //同上
         if (user!=null){
             System.out.println("用户存在");
             return 1;
@@ -65,9 +74,11 @@ public class login {
         //加密密码
         String ePassword = MD5Utils.MD5Lower(password,MD5Utils.salt);
 //        System.out.println(name+"  "+ePassword);
+        //i就是执行插入sql后影响的对象数目，如果插入了一个对象i就等于1 如果两个就是2
         int i = mapper.insertUser(name, ePassword);
         sqlSession.commit();
         sqlSession.close();
+
         if(i==1) {
             return 0;//成功返回
         }else {
--- src/main/java/loginSystem.java
+++ src/main/java/loginSystem.java
@@ -15,42 +15,55 @@ import java.util.Map;
 public class loginSystem extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String type = "sign";
-        String name = req.getParameter("sign_username");
-        String password = req.getParameter("sign_password");
+        String type = "sign";//定义一个操作类型 有sign=注册 login=登录
+        String name = req.getParameter("sign_username");//得到前端传来请求的sign_username参数
+        String password = req.getParameter("sign_password");//得到前端传来请求的sign_password参数
+
+        //如果sign_username这两个变量都是空的 那就是登录 login_username肯定是有值的 并且把type改成login
         if(name==null&&password==null){
             name = req.getParameter("login_username");
             password = req.getParameter("login_password");
             type = "login";
         }
 
+        //断言 防止name还是空的
         assert name != null;
+        //判断一下 防止只输入账号不输入密码的情况
         if(name.length()==0||password.length()==0){
             req.setAttribute("msg","信息核对失败，请检查");
             req.getRequestDispatcher("/index.jsp").forward(req,resp);
             return;
         }
 
+        //登录的逻辑
         if(type.equals("login")){
             System.out.println("进入登录");
+            //进去login类 验证账号密码
             boolean vaild = login.vaild(req.getParameter("login_username"), req.getParameter("login_password"));
 
+            //如果返回true就验证成功
             if (vaild){
-                String user = req.getParameter("login_username");
-                HttpSession session = req.getSession();
+                //这下面没什么讲的
+                String user = req.getParameter("login_username");//得到用户名
+                HttpSession session = req.getSession();//构建session会话等到主菜单有用
                 session.setAttribute("user",user);
                 //           System.out.println("登录成功");//登录成功操作
                 String contextPath = req.getContextPath();
+
+                //到这。。。。。
+                //重定向 进入主菜单
                 resp.sendRedirect(contextPath+"/main_menu/main.jsp");
             }
 
 
             else{
+                //登陆失败操作
                 System.out.println("登录失败");//登录失败操作
-                req.setAttribute("msg","用户名或密码错误");
+                req.setAttribute("msg","用户名或密码错误");//这里是传到登陆界面的提示信息，那边用msg作为变量名，后面的是参数
                 req.getRequestDispatcher("/index.jsp").forward(req,resp);
             }
 
+            //同上
         }else if(type.equals("sign")){
             int vaild = login.sign(name, password);
             if (vaild==0){
@@ -70,7 +83,7 @@ public class loginSystem extends HttpServlet {
         }
 
     }
-
+    //这个不用看
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("Get...");
